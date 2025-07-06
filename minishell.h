49e8de24b80d6f470bbfd80871fb86a686ba918c
NatimiDev <nati.mikula@gmail.com>
2025-07-06 19:45:10 +0200

    add basic logic for pwd, cd, env and echo

---
+++ minishell.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmikuka <nmikuka@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 13:34:42 by nmikuka           #+#    #+#             */
+/*   Updated: 2025/07/06 19:38:02 by nmikuka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+int		parse(char *cmd, char *envp[]);
+void	minishell_pwd(void);
+
+// libft functions
+int		ft_strncmp(const char *s1, const char *s2, int n);
+char	**ft_split(char const *s, char c);
+void	free_split(char	**split);
+
+#endif
